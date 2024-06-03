c5bb49eb91036710947c333da60d283c509ecb80
Pim van Milt <pim.milt@gmail.com>
2024-06-03 21:23:10 +0200

    working sorta

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  FormattedDate,
+  FormattedMessage,
+  IntlProvider,
+  useIntl,
+} from "react-intl";
+import { StyleSheet, Text, View, Button, useColorScheme } from "react-native";
 
 export default function App() {
+  function loadLocaleData(locale) {
+    switch (locale) {
+      case "nl":
+        return import("./compiled-lang/nl.json");
+      case "tr":
+        return import("./compiled-lang/tr.json");
+      default:
+        return import("./compiled-lang/en.json");
+    }
+  }
+
+  const changeLocale = () => {
+    switch (userLocale) {
+      case "en":
+        return setUserLocale("nl");
+      case "nl":
+        return setUserLocale("tr");
+      default:
+        return setUserLocale("en");
+    }
+  };
+  const [userLocale, setUserLocale] = useState(null);
+  const [localeMessages, setLocaleMessages] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const msg = await loadLocaleData(userLocale);
+      setLocaleMessages(msg);
+    })();
+  }, [userLocale]);
+
+  useEffect(() => {
+    setUserLocale("en");
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <IntlProvider locale={userLocale} messages={localeMessages}>
+      <View style={styles.container}>
+        <Text>Current Locale:</Text>
+        <Text>{userLocale}</Text>
+        <Text>Lets put up some text here:</Text>
+        <Text>
+          <FormattedMessage
+            description={"Welcome Message"}
+            defaultMessage={
+              "Welcome to my Tech Research into FormatJS. NOTE: This is the fallback message"
+            }
+          />
+        </Text>
+        <Text>And a date formatted according to the chosen localization:</Text>
+        <Text>
+          <FormattedDate value={Date.now()} />
+        </Text>
+        <Button title="Change Language" onPress={changeLocale} />
+        <StatusBar style="auto" />
+      </View>
+    </IntlProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+
+    plugins: [
+      [
+        "formatjs",
+        {
+          idInterpolationPattern: "[sha512:contenthash:base64:6]",
+          ast: true,
+        },
+      ],
+    ],
   };
 };
